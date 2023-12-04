01ae2a9ca156522c8c63115bf70b532c092029ec
Ayoub Chegraoui <ayoubch807@gmail.com>
2023-12-04 16:14:58 +0100

    DRY day4

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,37 +1,29 @@
-import java.util.*
-
 fun main() {
-
+    fun countWins(line: String): Int {
+        var res = 0
+        val winning: MutableSet<String> = HashSet()
+        val nums = line.split("\\s+".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        var i = 2
+        while (nums[i][0] != '|') winning.add(nums[i++])
+        while (i < nums.size) {
+            if (winning.contains(nums[i++])) res++
+        }
+        return res
+    }
 
     fun part1(lines: List<String>): Int {
         var ans = 0
         for (line in lines) {
-            var cnt = 0
-            val winning: MutableSet<String> = HashSet()
-            val nums = line.split("\\s+".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-            var i = 2
-            while (nums[i][0] != '|') winning.add(nums[i++])
-            while (i < nums.size) {
-                if (winning.contains(nums[i++])) cnt++
-            }
+            val cnt = countWins(line)
             ans += if (cnt == 0) 0 else 1 shl cnt - 1
         }
         return ans
     }
 
     fun part2(lines: List<String>): Int {
-        val occ = IntArray(lines.size)
-        Arrays.fill(occ, 1)
+        val occ = Array(lines.size) { 1 }
         for (i in lines.indices) {
-            val line = lines[i]
-            var cnt = 0
-            val winning: MutableSet<String> = HashSet()
-            val nums = line.split("\\s+".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-            var j = 2
-            while (nums[j][0] != '|') winning.add(nums[j++])
-            while (j < nums.size) {
-                if (winning.contains(nums[j++])) cnt++
-            }
+            val cnt = countWins(lines[i])
             for (k in 1..Math.min(cnt, lines.size - i - 1)) occ[i + k] += occ[i]
         }
         return occ.sum()
